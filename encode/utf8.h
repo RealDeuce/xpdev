926465c412dc0d37b883fd30705ff564ca5c01e5
rswindell <>
2019-07-08 00:07:23 +0000

    Moved cp437_unicode_tbl and unicode_is_zerowidth() to (new file) unicode.c. New function (derived from sbbs_t::utf8_to_cp437()): unicode_to_cp437() New utf8 functions: utf8_replace_chars(), utf8_str_is_valid(). utf8_getc() enhancement: val arg may be NULL (for length/validation uses).

--- encode/utf8.h
+++ encode/utf8.h
@@ -37,6 +37,7 @@
 #define UTF8_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #define UTF8_MAX_LEN 6	// Longest possible UTF-8 sequence
@@ -45,8 +46,13 @@
 extern "C" {
 #endif
 
+// Returns true if the string is valid UTF-8
+bool utf8_str_is_valid(const char*);
 // Normalizes (to ASCII) chars in UTF-8 string 'str', in-place, resulting in string <= original in length
 char* utf8_normalize_str(char* str);
+// Replace or strip UTF-8 sequences in str
+// If table ('tbl') of unicode codepoints if non-NULL is an array of 256 codepoints to map to 8-bit chars
+char* utf8_replace_chars(char* str, char (*lookup)(uint32_t), char unsupported_ch, char unsupported_zwch, char error_ch);
 // Decode a UTF-8 sequence to a UNICODE code point
 int utf8_getc(const char* str, size_t len, uint32_t* codepoint);
 // Encode a UNICODE code point into a UTF-8 sequence (str)
